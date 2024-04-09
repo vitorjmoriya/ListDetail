4deb40ccc049412c8314ff0d2b740f2bc7257bfa
Vitor Moriya <vitorjmoriya@gmail.com>
2024-04-08 21:14:53 -0300

    chore: first commit

---
+++ ListDetail/App.swift
@@ -0,0 +1,12 @@
+// App.swift
+
+import SwiftUI
+
+@main
+struct MyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ListDetail/ContentView.swift
@@ -0,0 +1,20 @@
+// ContentView.swift
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+        .background(Color.white)
+    }
+}
+
+#Preview {
+    ContentView()
+}
