5e18a59502553d93bcb3ffa39cdc58cb7e1c78c4
Vitor Moriya <vitorjmoriya@gmail.com>
2024-04-28 17:23:24 -0300

    feat(Card): add image

--- ListDetail/Component/Card.swift
+++ ListDetail/Component/Card.swift
@@ -13,25 +13,55 @@ struct Card: View {
 
     var body: some View {
         Button(action: onTap) {
-            VStack(alignment: .leading, spacing: 10) {
-                Text(viewModel.title)
-                    .font(.title2)
-                    .foregroundStyle(.black)
-                Text(viewModel.subtitle)
-                    .font(.caption)
-                    .foregroundStyle(.black)
+            renderContent()
+        }
+    }
+
+    @ViewBuilder private func renderContent() -> some View {
+        HStack(spacing: 10) {
+            renderImage()
+            renderInfo()
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding(.vertical, 10)
+        .padding(.leading, 20)
+        .background(Color.black.opacity(0.1))
+        .clipShape(RoundedRectangle(cornerRadius: 10))
+    }
+
+    @ViewBuilder private func renderImage() -> some View {
+        AsyncImage(url: viewModel.imageURL) { phase in
+            switch phase {
+            case .empty:
+                ProgressView()
+            case .success(let image):
+                image
+                    .resizable()
+                    .scaledToFit()
+            default:
+                Image(systemName: "exclamationmark.triangle")
+                    .foregroundStyle(.red)
             }
-            .frame(maxWidth: .infinity, alignment: .leading)
-            .padding(.vertical, 10)
-            .padding(.leading, 20)
-            .background(Color.black.opacity(0.1))
-            .clipShape(RoundedRectangle(cornerRadius: 10))
+        }
+        .clipShape(Circle())
+        .frame(width: 50, height: 50)
+    }
+
+    @ViewBuilder private func renderInfo() -> some View {
+        VStack(alignment: .leading, spacing: 10) {
+            Text(viewModel.title)
+                .font(.title2)
+                .foregroundStyle(.black)
+            Text(viewModel.subtitle)
+                .font(.caption)
+                .foregroundStyle(.black)
         }
     }
 }
 
 extension Card {
     struct ViewModel {
+        let imageURL: URL?
         let title: String
         let subtitle: String
     }
@@ -39,6 +69,7 @@ extension Card {
 
 #Preview {
     Card(viewModel: .init(
+        imageURL: nil,
         title: "Foobar",
         subtitle: "Lorem Ipsum"
     ), onTap: {})
